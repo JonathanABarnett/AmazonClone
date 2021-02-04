e3d7860667613c9e1f9d4680e0e286570317fd4a
Jonathan Barnett <jonathan.a.barnett@gmail.com>
2021-02-04 10:04:01 -0600

    Add Header Component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import "./App.css";
+import Header from "./components/header/Header";
 
 function App() {
   return (
     <div className="app">
-      <h1>Amazon Store Time </h1>
+      <Header />
+      {/* Home */}
     </div>
   );
 }
