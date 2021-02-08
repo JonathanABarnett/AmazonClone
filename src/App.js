22980b17628bc7319169f8329ea50e27786f5654
Jonathan Barnett <jonathan.a.barnett@gmail.com>
2021-02-08 09:09:52 -0600

    Build Login Page

--- src/App.js
+++ src/App.js
@@ -3,17 +3,22 @@ import Header from "./components/header/Header";
 import Home from "./components/home/Home";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Checkout from "./components/checkout/Checkout";
+import Login from "./components/login/Login";
 
 function App() {
   return (
     <Router>
       <div className="app">
-        <Header />
         <Switch>
           <Route exact path="/">
+            <Header />
             <Home />
           </Route>
+          <Route path="/login">
+            <Login />
+          </Route>
           <Route path="/checkout">
+            <Header />
             <Checkout />
           </Route>
         </Switch>
